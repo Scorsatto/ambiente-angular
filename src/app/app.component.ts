6c62bc269efead9fb13cf92959fbf721be257b40
Fabiano Scorsatto <fabianoscorsatto@gmail.com>
2022-07-17 10:02:18 -0400

    live de diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ambiente-angular';
+export class AppComponent implements OnInit {
+  count = 0;
+  text = '';
+  nome = 'Fabiano Scorsatto';
+
+  pessoas = [
+    {
+      nome: "Fabiano",
+      sobrenome: "Scorsatto"
+    },
+    {
+      nome: "Pessoa",
+      sobrenome: "2"
+    },
+    {
+      nome: "Terceira",
+      sobrenome: "Pessoa"
+    },
+    {
+      nome: "Ultimo",
+      sobrenome: "Humano"
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    let interval = setInterval(() => {
+      this.count++;
+
+      if (this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  clicou(nome: string): void {
+    console.log('clicou em mim', nome);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarcaTextoDirective } from './diretivas/marca-texto.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcaTextoDirective
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
